d01593713c3c8a6cc041ca1b4139dc22b606e24b
Rodrigo <rodrigo.melo199@gmail.com>
2018-05-20 09:38:54 -0300

    map

--- src/screens/Map.js
+++ src/screens/Map.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
-import { View, TextInput, TouchableOpacity, Text, StyleSheet } from 'react-native';
+import { View, TouchableOpacity, Text, StyleSheet, Dimensions, TextInput } from 'react-native';
 import { Location, Permissions } from 'expo';
+
+import mapStyle from '../assets/google-maps-style-config';
+
+const { width, height } = Dimensions.get('window');
+
+const shadow = {
+    shadowColor: '#000000',
+    shadowOffset: {
+    width: 0,
+    height: 3,
+    },
+    shadowRadius: 5,
+    shadowOpacity: 1.0,
+};
 
 const styles = StyleSheet.create({
     button: {
-        backgroundColor: '#000',
+        backgroundColor: '#fff',
+        padding: 12,
+        elevation: 3,        
+        alignItems: 'center',
+        width: '80%',
+        alignSelf: 'center',
     },
     view: {
         flex: 1,
@@ -13,14 +32,32 @@ const styles = StyleSheet.create({
     mapView: {
         flex: 1,
     },
-    textInput: {
-        marginTop: 20,
-        backgroundColor: '#f5deb3',
-
+    textInput: { 
+        marginTop: 30,
+        backgroundColor: '#fff',
+        height: 50,
+        marginHorizontal: 10,
+        paddingLeft: 15,
+        elevation: 3,
+        padding: 5,
     },
-
+    input2: {
+        marginTop: 10,
+        backgroundColor: '#fff',    
+        marginHorizontal: 10,    
+        height: 50,       
+        elevation: 3,
+        paddingLeft: 15,        
+        padding: 5,                
+    },
+    buttonContainer: {
+        flex: 1,
+        justifyContent: 'flex-end',
+    },
+    buttonLabel: {
+        color: '#04166A',
+    },
 });
-
 
 class Map extends Component {
     constructor(props) {
@@ -67,6 +104,7 @@ class Map extends Component {
                     onRegionChange={this.onRegionChange}
                     zoomEnabled={true}
                     scrollEnabled={true}
+                    customMapStyle={mapStyle}
                 >
                     <MapView.Marker
                         coordinate={this.state.location.coords}
@@ -75,28 +113,31 @@ class Map extends Component {
                     />
                 </MapView>
 
-                <View style={{ position: 'absolute' }}>
+                <View style={{ position: 'absolute', width, height, padding: 20, }}>
                     <TextInput
                         style={styles.textInput}
-                        placeHolder="from"
+                        placeholder="Origem"
                         onChangeText={this.handleChange('from')}
-                        value={this.state.from}>
-                    </TextInput>
+                        value={this.state.from}
+                        underlineColorAndroid="transparent"
+                    />
 
                     <TextInput
-                        style={styles.textInput}
-                        placeHolder="to"
+                        style={styles.input2}
+                        placeholder="Destino"
                         onChangeText={this.handleChange('to')}
-                        value={this.state.to}>
-                    </TextInput>
+                        value={this.state.to}
+                        underlineColorAndroid="transparent"                        
+                    />
 
-                    <TouchableOpacity style={styles.button}
-                    // onPress={}
-                    >
-                        <Text>
-                            Test
-                </Text>
-                    </TouchableOpacity>
+                   <View style={styles.buttonContainer}>
+                        <TouchableOpacity
+                            style={styles.button}
+                            // onPress={}
+                        >
+                            <Text style={styles.buttonLabel}>Confirmar rota</Text>
+                        </TouchableOpacity>
+                   </View>
                 </View>
 
             </View>
